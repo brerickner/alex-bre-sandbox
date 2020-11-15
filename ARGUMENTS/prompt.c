040ab82c85caf8a363ba013132f85ee8b83fd483
brerickner <breannarickner@gmail.com>
2020-11-15 05:02:02 +0000

    adding new prompt.c

--- ARGUMENTS/prompt.c
+++ ARGUMENTS/prompt.c
@@ -9,22 +9,36 @@ int print_string(char *str)
 int main (int ac, char **av)
 {
 	char prompt[] = "$ ";
-	char *buffer = " "; /*must be initialized or memory leaks present*/
+	char *buffer;
 	size_t buffSize = 0; /*initialized for memory leak purposes */
+	int gl;
 	(void)ac;
 
 	av[0] = prompt; /*assigns prompt to 0 index */
 	print_string(prompt); /*prints prompt $ */
 
-	buffer = malloc(strlen(buffer));
+	buffer = malloc(sizeof(char));
 	if (buffer == NULL)
 	{
 		perror("Unable to allocate memory");
 		exit(1);
 	}
-	free (buffer); /*freeing the byte used to initialize */
-      	getline(&buffer, &buffSize, stdin); /*reallocs to what it needs?*/
-	print_string(buffer);
-	free(buffer);
+
+	free(buffer); /*freeing the byte used to initialize */
+	gl = getline(&buffer, &buffSize, stdin); /*reallocs to what it needs?*/
+	if (gl == -1)
+	{
+		print_err("could not read line");
+		return (-1);
+	}
+	else
+	{
+		print_string(buffer);
+		free(buffer);
+	}
 	return (0);
+}
+int print_err(char *str)
+{
+	return (write(STDERR_FILENO, str, strlen(str)));
 }
